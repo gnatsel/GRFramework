54a9d8b9cb09120ac36116180c59f88a24d6f9ce
Olivier Lestang <olivierlestang@Oliviers-MacBook-Pro.local>
2016-07-28 11:43:06 +1000

    Removed Pods directory

--- Example/Pods/Target Support Files/GRFramework/GRFramework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GRFramework : NSObject
-@end
-@implementation PodsDummy_GRFramework
-@end
--- Example/Pods/Target Support Files/GRFramework/GRFramework-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double GRFrameworkVersionNumber;
-FOUNDATION_EXPORT const unsigned char GRFrameworkVersionString[];
-
